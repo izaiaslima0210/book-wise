d768fe2c6eab04a2a8b7ad35283860e4594daf9e
izaiaslima0210 <jrportorico@gmail.com>
2025-04-26 21:01:17 -0300

    refatorando classe conexão

--- models/conexao.php
+++ models/conexao.php
@@ -1,20 +1,30 @@
 <?php
-$db = new PDO('sqlite:' . __DIR__ . '/../database.sqlite');
 
-// Verifica se já existe a tabela 'user'
-$query = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='user';");
+class Conexao {
+    private static $instance = null;
 
-if ($query->fetch() === false) {
-    // Se não existe a tabela, cria todas
-    $path_sql = __DIR__ . '/../create_tables.sql';
-    $sql = file_get_contents($path_sql);
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new PDO('sqlite:' . __DIR__ . '/../database.sqlite');
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        $query = self::$instance->query("SELECT name FROM sqlite_master WHERE type='table' AND name='user';");
+        if ($query->fetch() === false) {
+            // Se não existe a tabela, cria todas
+            $path_sql = __DIR__ . '/../create_tables.sql';
+            $sql = file_get_contents($path_sql);
 
-    if (!$sql) {
-        die('Erro ao ler o arquivo create_tables.sql');
-    }
+            if (!$sql) {
+                die('Erro ao ler o arquivo create_tables.sql');
+            }
 
-    $db->exec($sql);
+            self::$instance->exec($sql);
+        }
+
+        return self::$instance;
+    }  
 }
 
-return $db;
+new Conexao();
 ?>
--- models/dados.php
+++ models/dados.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'conexao.php';
+
+$db = Conexao::getInstance();
 $query = "SELECT * FROM book";
 $livros = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
 
